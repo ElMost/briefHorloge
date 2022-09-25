8e498901178b5e89bea021c73409e99bfd7efacc
NASRI El Mostafa (SNCF VOYAGEURS / TECHNICENTRE PARIS ST LAZARE / Cellule Propreté) <elmost777@gmail.com>
2022-09-25 15:29:41 +0200

    calcul de degré du mouvement des aiguilles

--- js/script.js
+++ js/script.js
@@ -6,13 +6,21 @@ const AIGUILLESEC = document.querySelector("#second");
 //Extraire l'heure actuel à l'aide de l'objet Date()
 let curentTime = new Date();
 
-//Stocker l'heure , minute , seconde  dans des varaiables
+//Stocker l'heure , minute , seconde  dans des variables
 let hours = curentTime.getHours();
 let minutes = curentTime.getMinutes();
 let seconds = curentTime.getSeconds();
 
 // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
 // Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
+
+let hr = (hours * 60 * 60 + minutes * 60 + seconds) * (360 / 43200);
+let min = (minutes * 60 + seconds) * 0.1;
+let sec = seconds * 6;
+
+AIGUILLESEC.style.transform = `rotate(` + sec + `deg)`;
+AIGUILLEMIN.style.transform = `rotate(` + min + `deg)`;
+AIGUILLEHR.style.transform = `rotate(` + hr + `deg)`;
 
 // Déplacer les aiguilles
 function demarrerLaMontre() {}
